3a9a9a78f8892e2e89268ee8b4520c2f089ec7cc
tiestvangool <tiestvangool@gmail.com>
2016-11-18 09:44:07 -0700

    Initial commit

---
+++ chapter1/ch1-2ex1-4.c
@@ -0,0 +1,28 @@
+/*
+*   ch1-2ex1-4.c | Tiest van Gool
+*   Exersize 1-4 - Write a program to print the corresponding Celsius to
+*   Fahrenheit table.
+*/
+
+#include <stdio.h>
+
+#define LOWER 0         /* Lower limit of temperature table */
+#define UPPER 300       /* Upper limit of temperature table */
+#define STEP  20        /* Step size */
+
+int main(void)
+{
+  float fahr, celsius;
+
+  celsius = LOWER;
+
+  printf(" C      F \n");
+  printf("----------\n");
+
+  while (celsius <= UPPER) {
+    fahr = (celsius * 1.8) + 32;
+    printf("%3.0f %6.1f\n", celsius, fahr);
+    celsius = celsius + STEP;
+  }
+  return 0;
+}
---
+++ chapter1/ch1-3ex1-5.c
@@ -0,0 +1,26 @@
+/*
+*   ch1-3ex1-5.c | Tiest van Gool
+*   Exersize 1-5 - Modify the temperature conversion program to print the
+*   table in reverse order, that is, from 300 degrees to 0.
+*/
+
+#include <stdio.h>
+
+#define LOWER 0         /* Lower limit of temperature table */
+#define UPPER 300       /* Upper limit of temperature table */
+#define STEP  20        /* Step size */
+
+int main(void)
+{
+  float fahr, celsius;
+
+  fahr = UPPER;
+
+  printf(" F      C \n");
+  printf("----------\n");
+
+  for (fahr = UPPER; fahr >= LOWER; fahr = fahr - STEP) {
+    celsius = (5.0 / 9.0) * (fahr - 32.0);
+    printf("%3.0f %6.1f\n", fahr, celsius);
+  }
+}
