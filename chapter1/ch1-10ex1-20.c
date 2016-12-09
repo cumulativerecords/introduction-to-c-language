2f98b63cd8062a4b3e7994a61951c24126e9044d
tiestvangool <tiestvangool@gmail.com>
2016-12-09 09:29:36 -0700

    Initial commit

--- chapter1/ch1-10ex1-20.c
+++ chapter1/ch1-10ex1-20.c
@@ -1,57 +1,32 @@
 /*
-*   ch1-9ex1-19.c | Tiest van Gool
-*   Exersize 1-19 - Wtite a function reverse(s) that reverses the character
-*   strings. Use it to write a program that reverses its input one line at a
-*   time.
+*   ch1-10ex1-20.c | Tiest van Gool
+*   Exersize 1-20 - Wtite a program detab that replaces tabs in the input with
+*   the proper number of blanks to space to the next tab stop. Assume a fixed
+*   set of tab stops, say every n columns. Should n be a variable or symobolic
+*   parameter?
 */
 #include <stdio.h>
 
-#define MAXLINE 1000
-
-int gotline(char line[], int maxline);
-void swapline(char line[], int linelen);
+#define TABWIDTH 8
 
 int main(void) {
-  int len;
-  char line[MAXLINE];
-  char revline[MAXLINE];
+  int i, c, col, spaces;
+  int test0, test1, test2, test4;
+  float test3;
 
-  while ((len = gotline(line, MAXLINE)) > 0) {
-    if (line[0] != '\n') {
-      printf("len: %d\n", len);
-      swapline(line, len);
-      printf("reverse: %s", line);
+  col = 0;
+  while((c = getchar()) != EOF) {
+        if (c == '\t') {
+            spaces = TABWIDTH - col % TABWIDTH;
+            for (i = 0; i < spaces; ++i)
+                putchar(' ');
+            col = col + spaces;
+        } else {
+            putchar(c);
+            col = col + 1;
+            if (c == '\n')
+                col = 0;
+        }
+/*        printf("  spaces: %d, col: %d\n", spaces, col); */
     }
-  }
-}
-
-/* function getline: read line into s, return lenght */
-int gotline(char s[], int lim)
-{
-  int c, i;
-
-  for (i = 0; i <= lim-1 && (c = getchar()) != EOF && c != '\n'; i++) {
-    s[i] = c;
-  }
-  if (c == '\n') {
-    s[i] = c;
-    ++i;
-  }
-  s[i] = '\0';
-  return i;
-}
-
-/* function removeblank: remove trailing blanks and tabs from input line */
-void swapline(char s[], int len) {
-  int i;
-  char temp;
-
-  for (i = 0; i < len; i++) {
-    temp = s[i];
-    s[i] = s[len-1];
-    s[len-1] = temp;
-/* Print indexes and swapping begin and end values - nifty(!) */
-    printf("i: %d, len: %d, line: %s\n", i, len, s);
-    --len;
-  }
 }
